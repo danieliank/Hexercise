cae9b12267bb510dc87670bf363a43d2958f43ec
danieliank <daxter1835@gmail.com>
2024-05-31 15:29:27 +0700

    Initial Commit

---
+++ Hexercise Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hexercise Watch App
+//
+//  Created by Daniel Ian on 31/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hexercise Watch App/HexerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexerciseApp.swift
+//  Hexercise Watch App
+//
+//  Created by Daniel Ian on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Hexercise_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
