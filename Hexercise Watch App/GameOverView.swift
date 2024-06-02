52bf9c81c6ac7e202e58e1cae885a45b05a857d0
danieliank <daxter1835@gmail.com>
2024-06-03 05:37:52 +0700

    Initial Commit

---
+++ Hexercise Watch App/GameOverView.swift
@@ -0,0 +1,18 @@
+//
+//  GameOverView.swift
+//  Hexercise Watch App
+//
+//  Created by Daniel Ian on 02/06/24.
+//
+
+import SwiftUI
+
+struct GameOverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameOverView()
+}
---
+++ Hexercise Watch App/HeartRateManager.swift
@@ -0,0 +1,8 @@
+//
+//  HeartRateManager.swift
+//  Hexercise Watch App
+//
+//  Created by Daniel Ian on 31/05/24.
+//
+
+import Foundation
